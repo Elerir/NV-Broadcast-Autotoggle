14d4d0b18b8bac32cb8e7bbd7be02b7b7a330d90
Elerir <fkdslmfsdfds@dsjkgfdjklgfd.fr>
2021-09-08 14:59:10 +0200

    add missing ps1 files that actually tweak nvidia broadcast

---
+++ NvidiaBroadcastController.ps1
@@ -0,0 +1,52 @@
+$MethodDefinition = @'
+
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+[System.Runtime.InteropServices.DllImport("User32.dll", EntryPoint="ShowWindow")]
+public static extern bool ShowWindow(System.IntPtr hWnd, int nCmdShow);
+
+[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+public static extern IntPtr FindWindow(IntPtr sClassName, String sAppName);
+
+[DllImport("user32.dll")] 
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+'@
+
+$user32 = Add-Type -MemberDefinition $MethodDefinition -Name 'user32' -Namespace 'Win32' -PassThru
+
+
+function getDenoisingState(){
+	$value = $(Get-ItemProperty -path 'HKCU:\SOFTWARE\NVIDIA Corporation\NVIDIA Broadcast\Settings' -Name 'MicDenoising').MicDenoising
+	Write-Host "getDenoisingState $value"
+	return $value
+}
+
+function isDiscordRunning(){
+	$value = $(Get-Process | Where-Object { $_.Name -eq "discord" }).count
+	Write-Host "isDiscordRunning $value"
+	return $value 
+}
+
+function changeDenoisingState(){
+	Write-Host "changeDenoisingState"
+	$hwnd = $user32::FindWindow([IntPtr]::Zero, 'Nvidia BROADCAST')
+	if ($hwnd -eq 0){
+		Write-Host "cant find process"
+	}
+	$ret = $user32::PostMessage($hwnd, 0x0111, 16089198, 0);
+	$user32::ShowWindow($hwnd, 0)
+}
+
+
+if ($(isDiscordRunning)){
+	if (-Not $(getDenoisingState)){
+		changeDenoisingState
+	}
+}
+else{
+	if ($(getDenoisingState)){
+		changeDenoisingState
+	}
+}
